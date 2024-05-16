ce2b6a8047f20d71b91ef6e41faca83d1bfb4bef
Colson <colsoncalifornication@gmail.com>
2024-05-16 23:17:19 +0545

    use the context api to distribute data to a different component

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -5,16 +5,16 @@ import Card from '../UI/Card';
 import './ProductItem.css';
 import { toggleFav } from '../../store/actions/products';
 
-const ProductItem = props => {
-  const dispatch = useDispatch();
+const ProductItem = (props) => {
+  // const dispatch = useDispatch();
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
   };
 
   return (
     <Card style={{ marginBottom: '1rem' }}>
-      <div className="product-item">
+      <div className='product-item'>
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
         <button
--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useSelector } from 'react-redux';
 
 import ProductItem from '../components/Products/ProductItem';
+import { ProductsContext } from '../context/products-context';
 import './Products.css';
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+const Products = (props) => {
+  const productList = useContext(ProductsContext).products;
+  // const productList = useSelector((state) => state.shop.products);
+
   return (
-    <ul className="products-list">
-      {productList.map(prod => (
+    <ul className='products-list'>
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
